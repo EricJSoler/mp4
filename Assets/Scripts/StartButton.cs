ddc9e2606106b966f6a31897cbc26e29d8fb7340
Eric Soler <e.j.soler1@gmail.com>
2016-04-22 16:07:04 -0700

    ok i think?

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+	
+	public void ChangeScene(string sceneName) {
+        Application.LoadLevel(sceneName); // load the game scene
+        //Application.LoadLevel("level2"); // code correctly loads the game scene
+    }
+}
